e8d7b5c30075e668061c88f18e429b5b649ae4e3
kr1shnasharma <78134880+kr1shnasharma@users.noreply.github.com>
2022-08-30 02:16:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,101 @@
+// import React from "react";
+// import ReactDom from "react-dom";
+
+// ReactDom.render(<div>
+// <h1>Hello world</h1>
+// <p>this is paragraph</p>
+// </div>,
+// document.getElementById("root"))
+// import React from "react";
+// import ReactDom from "react-dom";
+// import "./styles.css";
+
+// ReactDom.render(<div>
+// <h1 className="heading" contentEditable="true" spellCheck="false">Hello world</h1>
+
+// <div>
+// <img className="img" src=""></img>
+// <img className="img" src=""></img>
+// </div>
+// </div>,
+// document.getElementById("root"))
+// import React from "react";
+// import ReactDom from "react-dom";
+// const fname="krishna";
+// const lname="sharma";
+// const number="20";
+// ReactDom.render(<div>
+// <h1>Hello {fname+" "+lname}</h1>
+// <h1>Hello {`${lname} ${fname}`}</h1>
+// <p>lucky number is {number}</p>
+// <p>lucky number is {Math.floor(Math.random()* 10)}</p>
+// </div>,
+// document.getElementById("root"))
+// import React from "react";
+// import ReactDom from "react-dom";
+// const name="krishna";
+// const year=new Date();
+// const year1=year.getFullYear();
+// const number="2019";
+// ReactDom.render(<div>
+
+// <p>Created by {name}</p>
+// <p>copyright {year1}</p>
+// </div>,
+// document.getElementById("root"))
+// import React from "react";
+// import ReactDom from "react-dom";
+// import "./styles.css";
+// const img="https://picsum.photos/200";
+// ReactDom.render(<div>
+// <h1 className="heading" contentEditable="true" spellCheck="false">Hello world</h1>
+
+// <div>
+// <img src={img}/>
+// </div>
+// </div>,
+// document.getElementById("root"))
+// import React from "react";
+// import ReactDom from "react-dom";
+// import "./styles.css";
+// const style={
+//   color:"blue",
+//   fontSize:"100px",
+//   border:"1px solid red"
+// }
+// ReactDom.render(<div>
+// <h1 style={style} className="heading" contentEditable="true" spellCheck="false">Hello world</h1>
+
+// <div>
+// </div>
+// </div>,
+// document.getElementById("root"))
+// import React from "react";
+// import ReactDom from "react-dom";
+// import "./styles.css";
+// const date=new Date();
+// const time=date.getHours();
+// const colour={
+// color:""
+// }
+// let greetings;
+// if(time<12){
+//   greetings="good morning"
+//   colour.color="red"
+// }
+// else if(time<18){
+//   greetings="good afternoon"
+//   colour.color="green"
+// }
+// else{
+//   greetings="good night"
+//   colour.color="blue"
+
+// }
+// ReactDom.render(<div>
+// <h1 className="heading" style={colour}>{greetings}</h1>
+
+// <div>
+// </div>
+// </div>,
+// document.getElementById("root"))
